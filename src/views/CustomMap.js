e3c4cab0daf275b898214a8d0aa16492d90c7a2a
Imad Rajwani <imadrajwani@gmail.com>
2018-08-11 19:54:08 +0500

    integrated directions polyline

--- src/views/CustomMap.js
+++ src/views/CustomMap.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Dimensions } from 'react-native'
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { Marker, Callout } from 'react-native-maps';
-//gmaps API key: "AIzaSyD2zYVyRTyNemGQtnrjsZnGGrR7R0knzMg"
+import MapViewDirections from 'react-native-maps-directions';
+
+const APIKey = "AIzaSyD2zYVyRTyNemGQtnrjsZnGGrR7R0knzMg";
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
 
 export default class CustomMap extends Component {
   constructor(props) {
@@ -10,6 +17,12 @@ export default class CustomMap extends Component {
     this.state = {};
   }
   render() {
+    //console.log((this.props.destinations.length > 2) ? this.props.destinations.slice(1, -1).latlng: null);
+    var coords = [];
+    this.props.destinations.map(
+      (data) => {coords.push(data.latlng);}
+    );
+    console.log(coords.length);
     return (
         <View style ={styles.container}>
         <MapView
@@ -18,12 +31,12 @@ export default class CustomMap extends Component {
 					}}
 					provider={PROVIDER_GOOGLE}
 					style={styles.map}
-                    region={{
-                            latitude: this.props.location.latitude,
-                            longitude: this.props.location.longitude,
-                            latitudeDelta: 0.015,
-                            longitudeDelta: 0.0121,
-                            }}
+          region={{
+                    latitude: this.props.location.latitude,
+                    longitude: this.props.location.longitude,
+                    latitudeDelta: LATITUDE_DELTA,
+                    longitudeDelta: LONGITUDE_DELTA,
+                    }}
         >
           <Marker
             coordinate={this.props.location}
@@ -39,6 +52,14 @@ export default class CustomMap extends Component {
             )
 
           )}
+          <MapViewDirections
+            origin={coords[0]}
+            destination={coords[coords.length - 1]}
+            waypoints={(coords.length > 2) ? coords.slice(1, -1): null}
+            apikey={APIKey}
+            strokeWidth={2}
+            strokeColor="#e83e35"
+          />
         </MapView>
       </View>
     )
